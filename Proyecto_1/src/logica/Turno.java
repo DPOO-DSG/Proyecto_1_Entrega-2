edcac4b99ce8887156dcac096584310a2bb079bc
5ebas2804 <j.reveloa@uniandes.edu.co>
2026-04-06 16:29:31 -0500

    Turno

--- Proyecto_1/src/logica/Turno.java
+++ Proyecto_1/src/logica/Turno.java
@@ -1,5 +1,27 @@
 package logica;
 
-public class Turno {
+import java.util.ArrayList;
 
+public class Turno {
+	private String jornada;
+	private ArrayList<Mesero> listaMeseros;
+	private ArrayList<Cocinero> listaCocineros;
+	//Constructor
+	public Turno(String jornada, ArrayList<Mesero> listaMeseros,
+			ArrayList<Cocinero> listaCocineros) {
+		this.jornada = jornada;
+		this.listaMeseros = listaMeseros;
+		this.listaCocineros = listaCocineros;
+	}
+	//Getters
+	public String getJornada() {
+		return jornada;
+	}
+	public ArrayList<Mesero> getListaMeseros() {
+		return listaMeseros;
+	}
+	public ArrayList<Cocinero> getListaCocineros() {
+		return listaCocineros;
+	}
+	
 }
