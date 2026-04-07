33b74ae5709c772a531dc79669218f8cf5fcb9fe
mgabrielah <mg.herrera@uniandes.edu.co>
2026-04-07 18:11:14 -0500

    Prestamo y juego

--- Proyecto_1/src/logica/InventarioPrestamo.java
+++ Proyecto_1/src/logica/InventarioPrestamo.java
@@ -1,5 +1,67 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class InventarioPrestamo {
+	private ArrayList<Juego> juegos;
+	private HashMap<Juego,Integer> stock;
+	//constructor
+		public InventarioPrestamo(ArrayList<Juego> juegos, HashMap<Juego, Integer> stock) {
+			super();
+			this.juegos = juegos;
+			this.stock = stock;
+		}
+
+		//getters and setters
+		public ArrayList<Juego> getJuegos() {
+			return juegos;
+		}
+
+		public void setJuegos(ArrayList<Juego> juegos) {
+			this.juegos = juegos;
+		}
+
+		public HashMap<Juego, Integer> getStock() {
+			return stock;
+		}
+
+		public void setStock(HashMap<Juego, Integer> stock) {
+			this.stock = stock;
+		}
+
+		public boolean estaDisponible(Juego juego) { 
+			// TODO Auto-generated method stub
+			return stock.containsKey(juego);
+		}
+
+		public boolean registrarPrestamo(Juego juego) {
+			// TODO Auto-generated method stub
+			if (!stock.containsKey(juego)) { 
+		        return false;
+		    } 
+
+		    int cantidad = stock.get(juego);
+
+		    if (cantidad == 1) {
+		        stock.remove(juego); //  llave juego desaparece
+		    } else {
+		        stock.put(juego, cantidad - 1);
+		    }
+
+		    return true;
+		    }
+		
+		public void devolverJuego(Juego juego) {
+
+		    if (stock.containsKey(juego)) {
+		        int cantidad = stock.get(juego);
+		        stock.put(juego, cantidad + 1);
+		    } else {
+		        stock.put(juego, 1); //  recrear la llave
+		    }
+		}
+		
+		
 
 }
--- Proyecto_1/src/logica/Juego.java
+++ Proyecto_1/src/logica/Juego.java
@@ -1,5 +1,128 @@
 package logica;
 
 public class Juego {
+	
+	private String categoria;
+	private String nombre;
+	private int cantidad;
+	private int añoPublicacion;
+	private String empresaMatriz;
+	private int minJugadores;
+	private int maxJugadores;
+	private String restriccionEdad;
+	private boolean dificl;
+
+	//Constructo
+	
+	public Juego(String categoria, String nombre, int cantidad, int añoPublicacion, String empresaMatriz,
+			int minJugadores, int maxJugadores, String restriccionEdad, boolean dificl) {
+		super();
+		this.categoria = categoria;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.añoPublicacion = añoPublicacion;
+		this.empresaMatriz = empresaMatriz;
+		this.minJugadores = minJugadores;
+		this.maxJugadores = maxJugadores;
+		this.restriccionEdad = restriccionEdad;
+		this.dificl = dificl;
+	}
+
+
+	//getters y setters
+	
+	
+	public boolean esDeAccion() {
+	    return this.categoria.equals("ACCION");
+	}
+	
+	public boolean isDificl() {
+		return dificl;
+	}
+	
+	public void setDificl(boolean dificl) {
+		this.dificl = dificl;
+	}
+
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+
+	public int getAñoPublicacion() {
+		return añoPublicacion;
+	}
+
+
+	public void setAñoPublicacion(int añoPublicacion) {
+		this.añoPublicacion = añoPublicacion;
+	}
+
+
+	public String getEmpresaMatriz() {
+		return empresaMatriz;
+	}
+
+
+	public void setEmpresaMatriz(String empresaMatriz) {
+		this.empresaMatriz = empresaMatriz;
+	}
+
+
+	public int getMinJugadores() {
+		return minJugadores;
+	}
+
+
+	public void setMinJugadores(int minJugadores) {
+		this.minJugadores = minJugadores;
+	}
+
+
+	public int getMaxJugadores() {
+		return maxJugadores;
+	}
+
+
+	public void setMaxJugadores(int maxJugadores) {
+		this.maxJugadores = maxJugadores;
+	}
+
+
+	public String getRestriccionEdad() {
+		return restriccionEdad;
+	}
+
+
+	public void setRestriccionEdad(String restriccionEdad) {
+		this.restriccionEdad = restriccionEdad;
+	}
+	
 
 }
